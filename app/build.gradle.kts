a542e6b0a9789ab1595a6b70a8b48189a8313ecd
Pablo Axel Rojas Hernandez <pabloaxel.rojas@gmail.com>
2025-04-15 10:37:15 -0600

    fix(build & layouts): Arregla error plugins y elimina fragments duplicados

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-
-    /*FIREBASE*/
-    id("com.android.application")
     // Add the Google services Gradle plugin
     id("com.google.gms.google-services")
 }
